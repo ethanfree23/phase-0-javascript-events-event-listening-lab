6f00dd83b8cce8fc10ae53613f30e1d11bccef5f
Ethan Freeman <ethanfree23@gmail.com>
2021-12-20 09:58:43 -0600

    commit tree 45dbf281103bd6bdebc86eee53ee103a0cf5c622

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 
 
-function addingEventListener() { // first, we create the function addingEventListener
-    const input = document.getElementById('input'); //getting element by ID, which is input (says it in the indextest.js)
-    input.addEventListener('click',()=>{ //attaching the input id to .addeventlistener, the specifying what action we want to listen for, then create an annonymous function for the event it invokes
-        alert = 'I\'ve been clicked!' //event being invoked is a string = alert = 'ive been clicked!'
+function addingEventListener(){
+    const input = document.querySelector('#input');
+    input.addEventListener('click', () => {
+        alert = "I\'ve been clicked."
     })
 }
+addingEventListener();
+
+
+// function addingEventListener(){
+//     const input = document.querySelector('#input');
+//     input.addEventListener("click", () => {
+//         alert = "I\'ve been clicked."
+//     })
+// }
+
 
 // function addingEventListener() {
-//     const input = document.getElementById('input');
-//     input.addEventListener('click', function() {
-//         alert("I was clicked!")
-//     });
+//     const input = document.querySelector('#input');
+//     input.addEventListener("click", () => {
+//         alert = "thing"
+//     })
 // }
+
+// function addingEventListener(){
+//     const input = document.querySelector("#input");
+//     input.addEventListener("click", () => {
+//         alert = ""
+//     })
+// }
+
+// function addingEventListener() { // first, we create the function addingEventListener
+//     const input = document.getElementById('input'); //getting element by ID, which is input (says it in the indextest.js)
+//     input.addEventListener('click',()=>{ //attaching the input id to .addeventlistener, the specifying what action we want to listen for, then create an annonymous function for the event it invokes
+//         alert = 'I\'ve been clicked!' //event being invoked is a string = alert = 'ive been clicked!'
+//     })
+// }
+
+// // function addingEventListener() {
+// //     const input = document.getElementById('input');
+// //     input.addEventListener('click', function() {
+// //         alert("I was clicked!")
+// //     });
+// // }
